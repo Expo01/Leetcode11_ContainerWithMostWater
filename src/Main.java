d3838ebcee217b24adfb1e4d1b88c78087a342e5
Expo <crdailey01@gmail.com>
2023-08-25 22:30:16 -0700

    brute force + optimized

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
+// 2 pointer
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+
+
+
+
+//brute force O(n^2)
+//class Solution {
+//    public int maxArea(int[] height) {
+//        int maxStorage = 0;
+//        for(int i = 0; i <height.length-1; i++){
+//            int j = i+1;
+//            while (j<height.length){
+//                int temp = Math.min(height[i],height[j]) * (j-i);
+//                if (temp > maxStorage){
+//                    maxStorage = temp;
+//                }
+//                j++;
+//            }
+//        }
+//        return maxStorage;
+//    }
+//}
